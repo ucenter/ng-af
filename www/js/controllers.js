b73f59343c83a0bfa7fae039f31f3a9714e127d1
zimzhang <zim-zhang@163.com>
2016-07-03 22:52:34 +0800

    camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -139,3 +139,31 @@ angular.module('app')
         }
     }
 }])
+
+.controller('paiCtrl',function($scope,$cordovaCamera){
+    $scope.title = '随手拍';
+
+    document.addEventListener("deviceready", function () {
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 100,
+            targetHeight: 100,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false,
+            correctOrientation:true
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // error
+        });
+
+    }, false);
+
+
+})
